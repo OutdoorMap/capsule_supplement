8ecab7f5e901becf5bf324200079093a1d4cf249
Joakim Nylen <git@joakim.nylen.nu>
2026-03-30 09:13:36 +0200

    fixeroo

--- lib/supplement/uploads/uri.ex
+++ lib/supplement/uploads/uri.ex
@@ -2,13 +2,13 @@ defimpl Capsule.Upload, for: URI do
   def contents(name) do
     download =
       Task.async(fn ->
-        :httpc.request(:get, {name |> URI.to_string() |> String.to_charlist(), []}, [],
+        :httpc.request(:get, {name |> URI.to_string() |> String.to_charlist(), [{'user-agent', 'capsule supplement elixir/v0.0.1'}]}, [],
           body_format: :binary
         )
       end)
 
     case download |> Task.await(15_000) do
-      {:ok, {{~c"HTTP/1.1", 200, ~c"OK"}, _headers, body}} ->
+      {:ok, {{~c"HTTP/1.1", 200, _}, _headers, body}} ->
         {:ok, body}
 
       {:ok, {{~c"HTTP/1.1", code, _}, _headers, _}} ->
